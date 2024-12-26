9dc2bd11004d77651d26adc943337fc464002d34
azwarnaim <azwardev@gmail.com>
2024-12-27 01:38:02 +0800

    UpdateClientFetchHOmePage

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,9 +1,29 @@
+// import HomePage from "./home";
+// import { getAllPosts } from "@/lib/sanity/client";
+
+// export default async function IndexPage() {
+//   const posts = await getAllPosts();
+//   return <HomePage posts={posts} />;
+// }
+
+// export const revalidate = 60;
+"use client"
+import { useEffect, useState } from "react";
 import HomePage from "./home";
 import { getAllPosts } from "@/lib/sanity/client";
 
-export default async function IndexPage() {
-  const posts = await getAllPosts();
+export default function IndexPage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const latestPosts = await getAllPosts();
+      setPosts(latestPosts);
+    };
+
+    fetchData();
+  }, []);
+
   return <HomePage posts={posts} />;
 }
 
-export const revalidate = 60;
