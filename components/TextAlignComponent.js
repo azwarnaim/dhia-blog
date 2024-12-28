76f50cdce6989c5d589817e141a3099f2d296ccd
azwarnaim <azwardev@gmail.com>
2024-12-28 16:58:40 +0800

    @UpdateRichtextEditor

---
+++ components/TextAlignComponent.js
@@ -0,0 +1,11 @@
+// TextAlignComponent.tsx
+
+import React from 'react'
+
+export const TextAlign = (props) => {
+    return (
+        <div style={{textAlign: props.value ? props.value : 'left', width: '100%'}}>
+            {props.children}
+        </div>
+    )
+}
